b8d35bf83e65c81a11a887883421920a5a0c19f4
Abdul Samad <sheikh.samad.dev@gmail.com>
2025-11-16 20:33:24 +0530

    Add files via upload

---
+++ samad.py
@@ -0,0 +1,15 @@
+ #print("Hello, Abdul Samad");
+# name = "samad"
+# print("hello",name)
+ #a = 4
+ #b = 9
+ #print("The sum is",a+b)
+ #name = input("what is your name? ")
+ #print("hello", name)
+ #a = int(input("Enter the first number:"))
+ #b = int(input("Enter the second number:"))
+ #print("addition", a+b)
+ #print("defference", a-b)
+print('''
+      this is the multi
+      ''')
