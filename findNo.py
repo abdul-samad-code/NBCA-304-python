0d7e70d53676d5fdf291c246fbe22f1bacb7bd69
Abdul Samad <sheikh.samad.dev@gmail.com>
2025-11-18 23:17:52 +0530

    Add files via upload

---
+++ findNo.py
@@ -0,0 +1,23 @@
+# a = int(input("Enter the first number:"))
+# b = int(input("Enter the second number"))
+# c = int(input("Enter the third number"))
+
+# if a >= b and b >= c:
+#     print("The greatest number is:",a)
+# elif b >= c:
+#     print("The greatest number is:",b)
+# else:
+#     print("The greatest number is:",c)
+
+
+
+a = int(input("Enter the first number:"))
+b = int(input("Enter the second number:"))
+c = int(input("Enter the third number:"))
+
+if a>=b and b>=c:
+    print("The greatest number is:",a)
+elif b>=c:
+    print("The greatest number is:",b)
+else:
+    print("The greatest number is:",c)
