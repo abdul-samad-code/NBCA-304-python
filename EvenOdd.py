d8771090d9cbfa8fc5f0ad918930302c9cbbfffb
Abdul Samad <sheikh.samad.dev@gmail.com>
2025-11-19 23:09:16 +0530

    Add files via upload

---
+++ EvenOdd.py
@@ -0,0 +1,19 @@
+num = int(input("Enter a number:"))
+if num % 2 == 0:
+    print("even")
+else:
+    print("odd")
+# num = int(input("Enter a number: "))
+# if num % 2 == 0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+
+
+
+num = int(input("Enter the input : "))
+if (num % 2 == 0):
+    print("--------even--------")
+else :
+    print("-----------odd-----------")
